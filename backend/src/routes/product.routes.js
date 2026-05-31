d0ddc12846f986012d5590b10eff0c623b37b946
Dhruva <dhruvatajapara@gmail.com>
2026-05-31 15:43:40 +0530

    implement create product api

--- backend/src/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createProduct } = require('../controllers/product/product.controller');
-
-router.post('/', createProduct);
-
-module.exports = router;
